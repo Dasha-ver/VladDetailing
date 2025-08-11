31fd49b6efcf1dd501bb74acbd57b494c3280245
Dasha-ver <ms.verishko@mail.ru>
2025-08-11 15:09:26 +0300

    modify admin

--- back/app/admin.py
+++ back/app/admin.py
@@ -1,3 +1,18 @@
+from .models import Service, ServiceDetail
 from django.contrib import admin
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+class ServiceDetailAdmin(admin.ModelAdmin):
+    list_display = ['id', 'service', 'name', 'price']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(ServiceDetail, ServiceDetailAdmin)
--- back/app/models.py
+++ back/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceDetail(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=300)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+from django.db import models
+
 # Create your models here.
